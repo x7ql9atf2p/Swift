abaf01a0c40f388ac746918ba0852ba4ec34b6b9
x7ql9atf2p <friendlytiger8392@protonmail.com>
2025-05-09 17:06:16 +0100

    Update messages.js

--- web/messages.js
+++ web/messages.js
@@ -1,1 +1,28 @@
+function createAccount(username) {
+  return fetch('/api/new', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: username,
+      authentication: 'examplepassword'
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success && data.token) {
+      return data.token;
+    } else {
+      throw new Error('Account creation failed or token missing.');
+    }
+  })
+  .catch(error => {
+    console.error('Error creating account:', error);
+    return null;
+  });
+}
 
+setTimeout(function() {
+  console.log(createAccount)
+}, 5000)
